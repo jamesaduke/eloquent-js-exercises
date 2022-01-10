73524c72ff153cee9e2f648c04879ac0eed2f55f
James Aduke <jamesodwuor4@gmail.com>
2022-01-10 21:26:24 +0300

    understaning map function 🐱‍👤

--- chapter-5-highOrderFns/highOrder.js
+++ chapter-5-highOrderFns/highOrder.js
@@ -3,47 +3,61 @@
 
 const SCRIPTS = require("./scripts");
 
-function repeat(n, action) {
-    for (let i = 0; i < n; i++) {
-        action(i);
-    }
-}
+// function repeat(n, action) {
+//     for (let i = 0; i < n; i++) {
+//         action(i);
+//     }
+// }
 
 
 // Below is a function that changes other function
-function noisy(f) {
-    return (...args) => {
-        console.log("calling with", args);
-        let result = f(...args);
-        console.log("called with", args, ", returned", result);
-        return result;
-    };
-}
+// function noisy(f) {
+//     return (...args) => {
+//         console.log("calling with", args);
+//         let result = f(...args);
+//         console.log("called with", args, ", returned", result);
+//         return result;
+//     };
+// }
 
-noisy(Math.min)(3,2,1);
+// noisy(Math.min)(3,2,1);
 
 // Functions that provide new type of control flow
 
-function unless(test, then) {
-    if (!test) then();
-}
+// function unless(test, then) {
+//     if (!test) then();
+// }
 
-repeat(3, n=>{
-    unless(n%2 == 1, () => {
-        console.log(n, "is even");
-    })
-});
+// repeat(3, n=>{
+//     unless(n%2 == 1, () => {
+//         console.log(n, "is even");
+//     })
+// });
 
 /* FILTERING ARRAY */
 
-function filter(array, test) {
-    let passed = [];
-    for (let element of array){
-        if (test(element)){
-            passed.push(element);
-        }
+// function filter(array, test) {
+//     let passed = [];
+//     for (let element of array){
+//         if (test(element)){
+//             passed.push(element);
+//         }
+//     }
+//     return passed;
+// }
+
+// console.log(filter(SCRIPTS, script => !script.living));
+
+// Filter is a standard array method
+
+/* Transforming arrays with map */
+function map(array, transform) {
+    let mapped = [];
+    for (let element of array) {
+        mapped.push(transform(element));
     }
-    return passed;
+    return mapped;
 }
 
-console.log(filter(SCRIPTS, script => !script.living));
+let rtlScript = SCRIPTS.filter(s => s.direction == "rtl");
+console.log(map(rtlScript, s => s.name));
