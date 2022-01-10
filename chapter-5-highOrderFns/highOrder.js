833d825c16d6e5a411ce081f145354d094f4a5ad
James Aduke <jamesodwuor4@gmail.com>
2022-01-10 21:10:05 +0300

    adding some high order examples

---
+++ chapter-5-highOrderFns/highOrder.js
@@ -0,0 +1,49 @@
+/* ABSTRACTION */
+// Abstracting repetition
+
+const SCRIPTS = require("./scripts");
+
+function repeat(n, action) {
+    for (let i = 0; i < n; i++) {
+        action(i);
+    }
+}
+
+
+// Below is a function that changes other function
+function noisy(f) {
+    return (...args) => {
+        console.log("calling with", args);
+        let result = f(...args);
+        console.log("called with", args, ", returned", result);
+        return result;
+    };
+}
+
+noisy(Math.min)(3,2,1);
+
+// Functions that provide new type of control flow
+
+function unless(test, then) {
+    if (!test) then();
+}
+
+repeat(3, n=>{
+    unless(n%2 == 1, () => {
+        console.log(n, "is even");
+    })
+});
+
+/* FILTERING ARRAY */
+
+function filter(array, test) {
+    let passed = [];
+    for (let element of array){
+        if (test(element)){
+            passed.push(element);
+        }
+    }
+    return passed;
+}
+
+console.log(filter(SCRIPTS, script => !script.living));
